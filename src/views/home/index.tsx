5bea899bad9da36bf5cb7959398832e62371fcbc
linhe <linhe@dtstack.com>
2021-01-05 17:47:07 +0800

    初始化子组件

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -7,7 +7,7 @@ import ChildTwo from './components/childClassTwo';
 import './style.scss';
 
 const { useState, useEffect } = React
-//
+//React的关于渲染的最重要的一个特性就是 当父组件重渲染的时候，其会默认递归的重渲染所有子组件
 export default () => {
   const [title, setTitle] = useState<string>('home');
   let count = 0;
